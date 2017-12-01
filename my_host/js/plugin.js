a19282b13c029e969b1978ebe208b0a1096cbac2
Aurélien CURTI <curti.aurelien@free.fr>
2017-12-01 18:04:55 +0100

    Ajout d'un ecouteur souris pour le son de l'oscillateur et mise en place du fichier audio en media source

--- my_host/js/plugin.js
+++ my_host/js/plugin.js
@@ -1,12 +1,38 @@
-var contexteAudio = new (window.AudioContext || window.webkitAudioContext)();
-var oscillateur = contexteAudio.createOscillator();
-var noeudGain = contexteAudio.createGain();
+var ctxAudio = new (window.AudioContext || window.webkitAudioContext)();
+
+//Source audio oscillateur
+var oscillateur = ctxAudio.createOscillator();
+
+var gainNode = ctxAudio.createGain();
 var lowpassFilter = 0;
-lowpassFilter = contexteAudio.createBiquadFilter();
-oscillateur.connect(noeudGain);
-oscillateur.connect(lowpassFilter);
-noeudGain.connect(contexteAudio.destination);
-lowpassFilter.connect(contexteAudio.destination);
+lowpassFilter = ctxAudio.createBiquadFilter();
+
+//Source audio musique
+var audio_input = new Audio();
+var source = ctxAudio.createMediaElementSource(audio_input);
+
+// Create variables to store mouse pointer Y coordinate
+// and HEIGHT of screen
+var CurY;
+var HEIGHT = window.innerHeight;
+// Get new mouse pointer coordinates when mouse is moved
+// then set new gain value
+document.onmousemove = updatePage;
+function updatePage(e) {
+	CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+	gainNode.gain.value = CurY/HEIGHT;
+	audio_input.volume = CurY/HEIGHT;
+}
+// connect the AudioBufferSourceNode to the gainNode
+// and the gainNode to the destination, so we can play the
+// music and adjust the volume using the mouse cursor
+
+
+source.connect(gainNode);
+oscillateur.connect(gainNode);
+gainNode.connect(lowpassFilter);
+lowpassFilter.connect(ctxAudio.destination);
+
 
 function set_oscillator_freq(){
 	oscillateur.frequency.value = document.querySelector("#oscillator_frequency").value;
@@ -19,3 +45,4 @@ function start_oscil(){
 function stop_oscil(){
 	oscillateur.stop();
 }
+
