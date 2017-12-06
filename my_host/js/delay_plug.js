7e9c52a45096d8ff380af7c32ecb38177e20130f
Elia Ditmann <elia.ditmann@etu.unice.fr>
2017-12-06 14:31:46 +0100

    ajout du plugin delay

---
+++ my_host/js/delay_plug.js
@@ -0,0 +1,58 @@
+//variables globales 
+var delayFilter;
+
+var delayDoc = document.currentScript.ownerDocument; 
+var delay_plugin = document.registerElement('delay-plugin', {
+    prototype: Object.create(HTMLElement.prototype, {
+		container: {                 // optionnel si on n'a pas besoin de valeur par défaut
+			value: "body",        // valeur par défaut de l'attribut name
+			writable: true,
+			enumerable: true,
+			configurable: true
+		},
+		createdCallback: { // exécuté à chaque création d'un élément <hello-world>
+			value: function() {
+			  var root = this.createShadowRoot();
+			  var template = delayDoc.querySelector('#hp_template'); // on cherche #template directement dans le DOM de hello-world.html
+			  var clone = document.importNode(template.content, true);
+			  var container = this.getAttribute("container"); //Data binding de la variable container
+
+			  clone.querySelector('#show_component').onclick = function() {getRender(container)};
+			  
+			  //Envoi d'evenement
+				this.dispatchEvent(new Event('add_plugin'));
+			  
+			  root.appendChild(clone);
+			}	
+		},
+		connect: {
+			value: function(ctx, nodeIn, nodeOut){
+				delayFilter = ctx.createDelay();
+				delayFilter.type = "delay";
+				nodeIn.connect(delayFilter);
+				delayFilter.connect(nodeOut);
+			}
+		},
+		disconnect: {
+			value: function(nodeIn, nodeOut){
+				delayFilter.disconnect(nodeIn);
+				delayFilter.disconnect(nodeOut);
+				nodeIn.connect(nodeOut);
+			}
+		},
+		getParams: {
+			value: function(){}
+		}
+	})
+  });
+
+function getRender(id_div_to_append){
+	var template_component = delayDoc.querySelector("#content_component");
+	document.querySelector(id_div_to_append).append(template_component);
+}
+
+function setDelay(val){
+	delayFilter.delayTime.value = parseInt(val);
+	document.querySelector("#delay_val").val = parseInt(val);
+}
+
