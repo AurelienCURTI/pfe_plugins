cecee922482ba27878848a49950beeaf48857ace
Elia Ditmann <elia.ditmann@etu.unice.fr>
2017-12-08 17:45:01 +0100

    ajout du flanger

---
+++ my_host/js/flanger_plug.js
@@ -0,0 +1,126 @@
+//variables globales 
+var flangerFilter;
+
+// The flanger produces a swirling effect by delaying a "copy" of the sound by a small, 
+//gradually changing period
+
+var flangerDoc = document.currentScript.ownerDocument; 
+var flanger_plugin = document.registerElement('flanger-plugin', {
+    prototype: Object.create(HTMLElement.prototype, {
+		
+		createdCallback: { // exécuté à chaque création d'un élément <hello-world>
+			value: function() {
+				var root = this.createShadowRoot();
+				var template = flangerDoc.querySelector('#hp_template'); // on cherche #template directement dans le DOM de hello-world.html
+				var clone = document.importNode(template.content, true);
+				var container = this.getAttribute("container"); //Data binding de la variable container
+
+				//Envoi d'evenement
+				var evt = flangerDoc.createEvent("CustomEvent");
+				evt.initCustomEvent("add_plugin", true, true, this);
+				this.dispatchEvent(evt);
+			  
+				root.appendChild(clone);
+			}	
+		},
+		connect: {
+			value: function(ctx, nodeIn, nodeOut){
+				//input
+				flangerInput = ctx.createGain();
+				//noeud du wetGain
+				flangerWetGainFilter = ctx.createGain();
+				flangerWetGainFilter.type = "flanger";
+				//noeud du delay
+				flangerDelayFilter = ctx.createDelay();
+				flangerDelayFilter.type = "flanger";
+				//noeud du gain
+				flangerGainFilter = ctx.createGain();
+				flangerGainFilter.type = "flanger";
+				//noeud du feedback
+				flangerFeedbackFilter = ctx.createGain();
+				//noeud de l'oscillateur
+				flangerOscilFilter = ctx.createOscillator();
+
+				//on connecte tout
+				nodeIn.connect(flangerInput);
+				flangerOscilFilter.connect(flangerGainFilter);
+				flangerGainFilter.connect(flangerDelayFilter.delayTime);
+				flangerInput.connect(flangerWetGainFilter);
+				flangerInput.connect(flangerDelayFilter);
+				flangerDelayFilter.connect(flangerWetGainFilter);
+				flangerDelayFilter.connect(flangerFeedbackFilter);
+				flangerFeedbackFilter.connect(flangerInput);
+				flangerWetGainFilter.connect(nodeOut);
+			}
+		},
+		disconnect: {
+			value: function(nodeIn, nodeOut){
+				flangerInput.disconnect(nodeIn);
+				flangerInput.disconnect(nodeOut);
+
+				flangerDelayFilter.disconnect(nodeIn);
+				flangerDelayFilter.disconnect(nodeOut);
+
+				flangerGainFilter.disconnect(nodeIn);
+				flangerGainFilter.disconnect(nodeOut);
+
+				flangerWetGainFilter.disconnect(nodeIn);
+				flangerWetGainFilter.disconnect(nodeOut);
+
+				flangerFeedbackFilter.disconnect(nodeIn);
+				flangerFeedbackFilter.disconnect(nodeOut);
+
+				flangerOscilFilter.disconnect(nodeIn);
+				flangerOscilFilter.disconnect(nodeOut);
+
+				nodeIn.connect(nodeOut);
+			}
+		},
+		getParams: {
+			value: function(){}
+		},
+		getRender: {
+			value: function(id_div_to_append){
+				var template_component = flangerDoc.querySelector("#content_component").innerHTML;
+				document.querySelector(id_div_to_append).innerHTML += template_component;
+			}
+		},
+		getPluginName:	{
+			value: function(){
+				return "flanger-plugin";
+			}
+		}
+	})
+  });
+
+// Changes the small delay time applied to the copied signal.
+// fonctionne avec le delay
+function setTime(val){
+	flangerDelayFilter.delayTime.value = parseFloat(val);
+	document.querySelector("#time_val").innerText = parseFloat(val);
+}
+
+// Changes the speed at which the flanging occurs.
+//fonctionne avec l'oscillateur
+function setSpeed(val){
+	flangerOscilFilter.frequency.value = parseFloat(val);
+	document.querySelector("#speed_val").innerText = parseFloat(val);
+}
+
+// Changes the depth/intensity of the swirling effect.
+function setDepth(val){
+	flangerGainFilter.gain.value = parseFloat(val);
+	document.querySelector("#depth_val").innerText = parseFloat(val);
+}
+
+// Changes the volume of the delayed sound.
+function setFeedback(val){
+	flangerFeedbackFilter.gain.value = parseFloat(val);
+	document.querySelector("#feedback_val").innerText = parseFloat(val);
+}
+
+// Volume balance between the original audio and the effected output.
+// function setMix(val){
+// 	flangerGainFilter.gain.value = parseFloat(val);
+// 	document.querySelector("#mix_val").innerText = parseFloat(val);
+// }
